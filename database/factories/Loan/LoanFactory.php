766f516e3365ee114257fc19dd7b6be1f41647a8
alirezaSalehi <aliunity751@gmail.com>
2025-09-14 02:52:13 -0700

    feat: create base model, controller, reuest,migrations

---
+++ database/factories/Loan/LoanFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Loan;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Loan\Loan>
+ */
+class LoanFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
