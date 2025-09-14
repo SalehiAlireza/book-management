915bec09e9d251bb9acc11ff67214d52e1bd1fbc
alirezaSalehi <aliunity751@gmail.com>
2025-09-14 02:58:47 -0700

    move model places

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Models\Models\Book;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Book extends Model
+class Category extends Model
 {
     use HasFactory;
 }
